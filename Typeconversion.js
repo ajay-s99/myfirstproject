58207150712e4993ff907d1661e8165b4c68a02c
Ajay Sutar <2ajaysutar@gmail.com>
2023-07-10 18:39:50 +0530

    Type conversion example

---
+++ Typeconversion.js
@@ -0,0 +1,6 @@
+let X= 6 + "" //here x is variable and have 2 diff types of data, num and string
+console.log(X,typeof X)
+
+let Y= 8
+Y=Y - 2 // Coercion- js change data type, here it changed strin y into num
+console.log(Y ,typeof Y)
